864aba40c93aee9f312ef2a778cfae2db8f71953
Dave Geddes <davidcgeddes@gmail.com>
2011-02-25 17:07:20 -0700

    added jQuery from CDN as a dependency for the app

--- components/main.js
+++ components/main.js
@@ -1,2 +1,5 @@
-console.log("app loaded");define("app", function(){});
-define("main", function(){});
+define('main',["https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"], function(){
+   $(document).ready(function(){
+        console.log("dom ready");
+    });
+});
