9680efc089bb4984ca6eb46293fbfb1bcfcac133
Dave Geddes <davidcgeddes@gmail.com>
2011-03-07 23:03:12 -0700

    created control.js module for controlling DOM elements

--- components/tests.js
+++ components/tests.js
@@ -1,10 +1,30 @@
-define(["https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", "qunit"], function(qunitNull){
+define(["https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", "qunit", "utils/control"], function(jQueryNull, qunitNull, control){
     $(document).ready(function(e){
         //sample test
         test("sample test", function() {
           ok( true, "this test is fine" );
           var value = "hello";
           equals( "hello", value, "We expect value to be hello" );
+        });
+
+        //test control.js
+        test("mapFields test", function(){
+            //put some html into the dom
+            var html = "<div data-field=title>My Title</div>";
+            var elem = $('body').append(html);
+            var module = {
+                title: ''
+            }
+            //enable controlling
+            control.mapFields(module, elem, ['title']);
+
+            //test changing text value
+            module.title.text('New Title');
+            equals("New Title", module.title.text(), "Title should be 'New Title'");
+
+            //test changing css
+            module.title.css('color', 'rgb(0, 255, 0)');
+            equals("rgb(0, 255, 0)", module.title.css('color'), "color should be rgb(0, 255, 0)");
         });
     });
 });
---
+++ components/utils/control.js
@@ -0,0 +1,14 @@
+define(function(){
+    return {
+        //generic method for mapping fields to be controlled
+        mapFields: function(object, element, fields){
+            var field
+            for (var i = 0, length = fields.length, field; i < length; i +=1) {
+                field = fields[i];
+                object[field] = element.find('[data-field]='+field).removeAttr('data-field');
+            }
+        }
+    }
+});
+
+
